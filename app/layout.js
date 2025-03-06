ba5486ccac75008932c4c8ef0df639f4073ac6fc
Akash00799 <bhardwaj.akash1199@gmail.com>
2025-03-07 03:22:07 +0530

    intigrate clerkProvider for signUp and signIn page  and create ThemeProvider for dark mode

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,10 @@
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { defaultTheme } from "@clerk/themes";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Deepak-studio",
@@ -7,8 +13,27 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={""}>{children}</body>
-    </html>
+    <ClerkProvider appearance={{ baseTheme: defaultTheme }}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <header>Hello</header>
+
+            <main className="min-h-screen">{children}</main>
+
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with love by Akash</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
